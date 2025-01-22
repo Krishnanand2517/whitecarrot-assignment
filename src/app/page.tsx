f90a976b8eff7427e848b64eac32883a18427d2a
Krishnanand Yadav <krishnanand2517@gmail.com>
2025-01-23 01:32:01 +0530

    feat: Implement sign in with google and sign out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import { createClient } from "@/utils/supabase/server";
 import OAuthButton from "@/components/OAuthButton";
+import UserProfile from "@/components/UserProfile";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = await createClient();
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-6">
-      <OAuthButton />
+      {session ? <UserProfile user={session.user} /> : <OAuthButton />}
     </div>
   );
 }
