103cca2636cf7d0db507a63c2c51e7a196682f8f
Krishnanand Yadav <krishnanand2517@gmail.com>
2025-01-23 16:54:50 +0530

    style: Animate the OAuth button, make dark mode permanent

--- src/components/OAuthButton.tsx
+++ src/components/OAuthButton.tsx
@@ -22,7 +22,7 @@ const OAuthButton = () => {
     <div>
       <button
         onClick={handleOAuth}
-        className="px-6 py-2 border border-slate-300 hover:bg-slate-800 rounded-md flex gap-4 transition-colors"
+        className="px-6 py-2 hover:scale-105 rounded-md flex gap-4 transition-all animate-gradient-slow bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-[length:200%_200%] text-white"
       >
         <Image
           src={GoogleLogo}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,16 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      animation: {
+        "gradient-slow": "gradient 3s infinite ease-in-out",
+      },
+      keyframes: {
+        gradient: {
+          "0%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+          "100%": { backgroundPosition: "0% 50%" },
+        },
+      },
     },
   },
   plugins: [],
